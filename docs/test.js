7694a580c246a2e9ca1a66ec3efdfb44e1a2b74c
astrawnuts <52769735+astrawnuts@users.noreply.github.com>
2021-08-15 20:55:22 +0530

    see below added an option to return year end only

--- docs/test.js
+++ docs/test.js
@@ -1,78 +1,72 @@
-import React from 'react'
+import React from "react";
 
-export default function test() {
-   
-/**
- * Test Cases allows you to define any day month year
- */
+export default function test(props) {
+  /**
+   * Test Cases allows you to define any day month year
+   */
 
-// const { deflateSync } = require("zlib");
+  // const { deflateSync } = require("zlib");
 
-// let day = undefined;
-// let month = undefined;
-// let year = undefined;
+  // let day = undefined;
+  // let month = undefined;
+  // let year = undefined;
 
-const period_start = "01/04";
-const period_end = "31/03";
+  const period_start = "01/04";
+  const period_end = "31/03";
 
-//current year variables
-let current_financial_period_start = period_start;
-let current_financial_period_end = period_end;
+  //current year variables
+  let current_financial_period_start = period_start;
+  let current_financial_period_end = period_end;
 
-//last year variables
-let last_year_financial_period_start = period_start;
-let last_year_financial_period_end = period_end;
+  //last year variables
+  let last_year_financial_period_start = period_start;
+  let last_year_financial_period_end = period_end;
 
-
-//checks if a value is a number and 
-//tries to convert to a num
-function tryParseNumber(num) {
-    if(typeof num == 'number') {
-        return num;
-    } 
-    
-    if(typeof num != 'string') {
-        return -1;
+  //checks if a value is a number and
+  //tries to convert to a num
+  function tryParseNumber(num) {
+    if (typeof num === "number") {
+      return num;
     }
-    
+
+    if (typeof num !== "string") {
+      return -1;
+    }
+
     try {
-            num = parseInt(num);
-            return num;
-        } catch (e) {
-            return -1;
-        }
-    
-}
+      num = parseInt(num);
+      return num;
+    } catch (e) {
+      return -1;
+    }
+  }
 
-//gets the current year or returns a the passed number
-function getYear(year) {
+  //gets the current year or returns a the passed number
+  function getYear(year) {
     year = tryParseNumber(year);
-    return ((year === -1 ) ? new Date().getFullYear() : year);
-}
+    return year === -1 ? new Date().getFullYear() : year;
+  }
 
-//gets the current month or returns a the passed number
-function getMonth(month) {
+  //gets the current month or returns a the passed number
+  function getMonth(month) {
     month = tryParseNumber(month);
-    return ((month === -1 ) ? new Date().getMonth() + 1 : month);
-}
+    return month === -1 ? new Date().getMonth() + 1 : month;
+  }
 
-//gets the current day or returns a the passed number
-function getDay(day) {
+  //gets the current day or returns a the passed number
+  function getDay(day) {
     day = tryParseNumber(day);
-    return ((day === -1 ) ? new Date().getDate() : day);
-}
+    return day === -1 ? new Date().getDate() : day;
+  }
 
-
-
-function getCurrentFinancialYear(day, month, year) {
+  function getCurrentFinancialYear(day, month, year) {
     /**
-     * Current Year 
+     * Current Year
      * Calculations
      */
     const current_year = getYear(year);
     const current_month = getMonth(month);
     const current_day = getDay(day);
-
 
     let current_year_start = null;
     let current_year_end = null;
@@ -81,76 +75,86 @@ function getCurrentFinancialYear(day, month, year) {
     //Current Financial Year
     //if currently we are in the month of April the next financial year has begun
     if (current_month >= 4) {
-        current_year_start = current_year;
-        current_year_end = current_year + 1;
+      current_year_start = current_year;
+      current_year_end = current_year + 1;
     }
 
     if (current_month <= 3) {
-        current_year_start = (current_year - 1)
-        current_year_end = (current_year);
+      current_year_start = current_year - 1;
+      current_year_end = current_year;
     }
 
     console.log(current_year_start);
 
-    current_financial_period_start = current_financial_period_start + "/" + current_year_start;  
-    current_financial_period_end = current_financial_period_end + "/" + current_year_end; 
+    current_financial_period_start =
+      current_financial_period_start + "/" + current_year_start;
+    current_financial_period_end =
+      current_financial_period_end + "/" + current_year_end;
 
     //creates a financial period string
-    current_financial_period_string = current_financial_period_start + " to " + current_financial_period_end;
+    current_financial_period_string =
+      current_financial_period_start + " to " + current_financial_period_end;
     return current_financial_period_string;
+  }
 
-}
+  function getLastFinancialYear(day, month, year, EndPeriodOnly = false) {
+    /*******************************
+     *
+     * last years financial period string
+     */
 
-function getLastFinancialYear(day, month, year) {
+    const current_year = getYear(year);
+    const current_month = getMonth(month);
+    const current_day = getDay(day);
 
-/********************************
- * 
- * last years financial period string
- */
+    //last year variables
+    let last_year_financial_period_string = null;
+    last_year_financial_period_start += "/" + (current_year - 1);
+    last_year_financial_period_end += "/" + current_year;
 
- const current_year = getYear(year);
- const current_month = getMonth(month);
- const current_day = getDay(day);
+    last_year_financial_period_string =
+      last_year_financial_period_start +
+      " to " +
+      last_year_financial_period_end;
 
-//last year variables
-let last_year_financial_period_string = null;
-last_year_financial_period_start += "/" + (current_year - 1);
-last_year_financial_period_end += "/" + current_year;
+    if (props.YearEndOnly) {
+      return last_year_financial_period_end;
+    }
 
-last_year_financial_period_string = last_year_financial_period_start + " to " + last_year_financial_period_end;
+    return last_year_financial_period_string;
+  }
 
-return last_year_financial_period_string;
+  // console.log(getCurrentFinancialYear(undefined, undefined, undefined));
+  // console.log(getCurrentFinancialYear(undefined, undefined, undefined));
 
-}
+  // module.exports.getCurrentFinancialYear = getCurrentFinancialYear;
+  // // module.exports.getLastFinancialYear = getLastFinancialYear;
 
+  // function updateText() {
+  // const year1 = document.querySelector(".year1");
 
-// console.log(getCurrentFinancialYear(undefined, undefined, undefined));
-// console.log(getCurrentFinancialYear(undefined, undefined, undefined));
+  // let time = new Date();
 
-// module.exports.getCurrentFinancialYear = getCurrentFinancialYear;
-// // module.exports.getLastFinancialYear = getLastFinancialYear;
+  // year1.innerText = getCurrentFinancialYear();
 
-function updateText() {
-const year1 = document.querySelector(".year1");
+  // const year2 = document.querySelector(".year2");
 
-let time = new Date();
+  // year2.innerText = getLastFinancialYear();
+  // }
 
-year1.innerText = getCurrentFinancialYear();
+  // document.addEventListener('DOMContentLoaded', updateText, false);
 
-const year2 = document.querySelector(".year2");
+  return (
+    <>
+      {props.YearEndOnly === undefined && (
+        <span>
+          Previous Financial Year: {getLastFinancialYear(null, null, null)}
+        </span>
+      )}
 
-year2.innerText = getLastFinancialYear();
-}
-
-// document.addEventListener('DOMContentLoaded', updateText, false);
-
-
-
-
-
-    return (
-        <>
-            <span>Previous Financial Year: {getLastFinancialYear(null, null, null)}</span>
-        </>
-    )
+      {props.YearEndOnly !== undefined && (
+        <span>{getLastFinancialYear(null, null, null)}</span>
+      )}
+    </>
+  );
 }
