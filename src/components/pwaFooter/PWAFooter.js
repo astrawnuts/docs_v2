e821959d0bcf05e010976977e0d788dd7f659405
astrawnuts <astrawnuts@gmail.com>
2021-12-25 03:46:00 +1300

    tab bar created for pwa

---
+++ src/components/pwaFooter/PWAFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "react-onsenui"; // Only import the necessary components
+
+export default function PWAFooter() {
+  return (
+    <>
+        <div className="tab-bar">
+            <div className="tab">
+              hello
+            </div>
+        </div>
+    </>
+  );
+}
+
