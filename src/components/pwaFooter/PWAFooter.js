aa1797564dc3da43dc30ce9652064f890078f50e
astrawnuts <astrawnuts@gmail.com>
2021-12-25 20:12:23 +1300

    focus on click

--- src/components/pwaFooter/PWAFooter.js
+++ src/components/pwaFooter/PWAFooter.js
@@ -60,7 +60,7 @@ export default function PWAFooter() {
                   document
                     .querySelector("button.aa-DetachedSearchButton")
                     .click();
-                  document.querySelector("input#autocomplete-14-input").click();
+                  document.querySelector("input#autocomplete-14-input").focus();
                 }}
               >
                 <svg
