26d7c825177d2e3639379da4df8a6b56ad5e1967
astrawnuts <astrawnuts@gmail.com>
2021-12-26 00:04:21 +1300

    pwa share functionality

--- src/components/pwaFooter/PWAFooter.js
+++ src/components/pwaFooter/PWAFooter.js
@@ -8,12 +8,33 @@ export default function PWAFooter() {
     document.documentElement.scrollTop = 0;
   }
 
+  function sharePage() {
+    if (navigator.share) {
+
+      let currentPage = window.location.href.toString();
+      let currentPageTitle = document.title.toString();
+
+      navigator.share({
+        title: currentPageTitle,
+        text: 'Hey You, Check out this page on Help Desk by Tax Matterz ' + currentPageTitle + " | ",
+        url: currentPage,
+      })
+
+      .then(() => console.log('Share was successful.'))
+      .catch((error) => console.log('Sharing failed', error));
+    } else {
+      console.log(`Your system doesn't support sharing files.`);
+    }
+    
+  }
+
   return (
     <>
       <div className="tab-bar">
         <div className="container">
           <div className="row-bar">
             <div className="col-bar">
+              {/* Articles Button */}
               <div
                 className="icon"
                 onClick={() => {
@@ -48,27 +69,9 @@ export default function PWAFooter() {
               </div>
               <div className="label">Articles</div>
             </div>
+    
             <div className="col-bar">
-              <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-airplay"
-                >
-                  <path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path>
-                  <polygon points="12 15 17 21 7 21 12 15"></polygon>
-                </svg>
-              </div>
-              <div className="label">hello</div>
-            </div>
-            <div className="col-bar">
+              {/* Search Button Below */}
               <div
                 className="icon"
                 onClick={() => {
@@ -96,23 +99,18 @@ export default function PWAFooter() {
               <div className="label">Search</div>
             </div>
 
-            <div className="col-bar" onClick={() => topFunction()}>
+            <div className="col-bar" onClick={() => sharePage()}>
+              {/* Share Button Below */}
               <div className="icon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-arrow-up"
-                >
-                  <line x1="12" y1="19" x2="12" y2="5"></line>
-                  <polyline points="5 12 12 5 19 12"></polyline>
-                </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg>
+              </div>
+              <div className="label">Share</div>
+            </div>
+
+            <div className="col-bar" onClick={() => topFunction()}>
+              {/* Scroll To Top Button */}
+              <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="16 12 12 8 8 12"></polyline><line x1="12" y1="16" x2="12" y2="8"></line></svg>
               </div>
               <div className="label">Top</div>
             </div>
