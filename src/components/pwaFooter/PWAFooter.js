80f00a2867074780600b355ca2808eb366a96584
astrawnuts <astrawnuts@gmail.com>
2021-12-26 22:15:19 +1300

    pwa functionality updated

--- src/components/pwaFooter/PWAFooter.js
+++ src/components/pwaFooter/PWAFooter.js
@@ -11,22 +11,24 @@ export default function PWAFooter() {
   //share current page on mobile
   function sharePage() {
     if (navigator.share) {
-
       let currentPage = window.location.href.toString();
       let currentPageTitle = document.title.toString();
 
-      navigator.share({
-        title: currentPageTitle,
-        text: 'Hey You, Check out this page on Help Desk by Tax Matterz ' + currentPageTitle + " | ",
-        url: currentPage,
-      })
+      navigator
+        .share({
+          title: currentPageTitle,
+          text:
+            "Hey You, Check out this page on Help Desk by Tax Matterz " +
+            currentPageTitle +
+            " | ",
+          url: currentPage,
+        })
 
-      .then(() => console.log('Share was successful.'))
-      .catch((error) => console.log('Sharing failed', error));
+        .then(() => console.log("Share was successful."))
+        .catch((error) => console.log("Sharing failed", error));
     } else {
       console.log(`Your system doesn't support sharing files.`);
     }
-    
   }
 
   return (
@@ -34,53 +36,55 @@ export default function PWAFooter() {
       <div className="tab-bar">
         <div className="container">
           <div className="row-bar">
-            <div className="col-bar">
-              {/* Articles Button */}
-              <div
-                className="icon"
+            <Link to="/">
+              <button
+                className="col-bar __active"
                 onClick={() => {
                   let width = window.innerWidth;
                   console.log(width);
                   //if app is in mobile mode (without sidebar)
                   if (width <= 996) {
+                    event.preventDefault();
                     document.querySelector("button.navbar__toggle").click();
                   } else {
-                    window.location.href = "/";
+                    // window.location.href = "/";
                   }
 
                   //if desktop view is showing
+                }}
+              >
+                {/* Articles Button */}
+                <div className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-book-open"
+                  >
+                    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                    <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                  </svg>
+                </div>
+                <div className="label">Articles</div>
+              </button>
+            </Link>
 
-                }}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="feather feather-book-open"
-                >
-                  <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                  <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-                </svg>
-              </div>
-              <div className="label">Articles</div>
-            </div>
-    
-            <div className="col-bar">
+            <button
+              className="col-bar"
+              onClick={() => {
+                document
+                  .querySelector("button.aa-DetachedSearchButton")
+                  .click();
+              }}
+            >
               {/* Search Button Below */}
-              <div
-                className="icon"
-                onClick={() => {
-                  document
-                    .querySelector("button.aa-DetachedSearchButton")
-                    .click();
-                }}
-              >
+              <div className="icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -98,23 +102,52 @@ export default function PWAFooter() {
                 </svg>
               </div>
               <div className="label">Search</div>
-            </div>
+            </button>
 
-            <div className="col-bar" onClick={() => sharePage()}>
+            <button className="col-bar" onClick={() => sharePage()}>
               {/* Share Button Below */}
               <div className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-share"
+                >
+                  <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
+                  <polyline points="16 6 12 2 8 6"></polyline>
+                  <line x1="12" y1="2" x2="12" y2="15"></line>
+                </svg>
               </div>
               <div className="label">Share</div>
-            </div>
+            </button>
 
-            <div className="col-bar" onClick={() => topFunction()}>
+            <button className="col-bar" onClick={() => topFunction()}>
               {/* Scroll To Top Button */}
               <div className="icon">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="16 12 12 8 8 12"></polyline><line x1="12" y1="16" x2="12" y2="8"></line></svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-corner-right-up"
+                >
+                  <polyline points="10 9 15 4 20 9"></polyline>
+                  <path d="M4 20h7a4 4 0 0 0 4-4V4"></path>
+                </svg>
               </div>
-              <div className="label">Top</div>
-            </div>
+              <div className="label">Go Top</div>
+            </button>
           </div>
         </div>
       </div>
