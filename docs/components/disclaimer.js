795eeec019f1a76c06115cf10d3740de5d04fe29
astrawnuts <astrawnuts@gmail.com>
2022-01-10 23:57:09 +1300

    disclaimer details updated

--- docs/components/disclaimer.js
+++ docs/components/disclaimer.js
@@ -5,7 +5,7 @@ export default function Disclaimer() {
     return (
         <div>
             <p className="italic">
-                While we have made every attempt ensure that the information on this website is accurate we advise you <strong>NOT to act</strong> solely based on this information alone. Always consult professional such as a tax agent and or <Link to="https://taxmatterz.com/contactus"><strong>tax advisor of Tax Matterz based in Auckland, New Zealand</strong></Link> (formerly Tax Matters) as each person(s) situation and circumstances may vary. <Link to="/disclaimer"> Please read our detailed disclaimer here.</Link>
+                While we have made every attempt ensure that the information on this website is accurate we advise you <strong>NOT to act</strong> solely based on this information alone. Always consult professional such as a tax agent and or <Link to="https://taxmatterz.com/contactus"><strong>tax advisor of Tax Matterz based in Auckland, New Zealand</strong></Link> (formerly Tax Matters) as each person(s) situation and circumstances may vary. <Link to="/disclaimer"> Please read our detailed disclaimer here.</Link> 
             </p>
    
         </div>
