47e3d1566243d375d3ce504cd0cb64f815cfd71b
astrawnuts <astrawnuts@gmail.com>
2022-01-11 02:36:58 +1300

    className replaced with class

--- docs/components/disclaimer.js
+++ docs/components/disclaimer.js
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 export default function Disclaimer() {
     return (
         <div>
-            <p className="italic">
+            <p class="italic">
                 While we have made every attempt ensure that the information on this website is accurate we advise you <strong>NOT to act</strong> solely based on this information alone. Always consult professional such as a tax agent and or <Link to="https://taxmatterz.com/contactus"><strong>tax advisor of Tax Matterz based in Auckland, New Zealand</strong></Link> (formerly Tax Matters) as each person(s) situation and circumstances may vary. <Link to="/disclaimer"> Please read our detailed disclaimer here.</Link> 
             </p>
    
